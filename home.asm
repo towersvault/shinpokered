0037a7502810db075b30269420c1a32147d76125
jojobear13 <47074297+jojobear13@users.noreply.github.com>
2020-11-11 15:59:18 -0600

    Update audio engine to yellow version (#63)
    * initial changes for audio engine
    
    * audio engine 1 modification
    
    * audio engine 2 partial modifications
    
    * audio engine 2 even more modifications
    
    * audio engine 3 modifications and engine 1 fix
    
    * finish updating audio engine to yellow version
    
    * SELECT on option screen changes sound setting

--- home.asm
+++ home.asm
@@ -1051,9 +1051,7 @@ FadeOutAudio::
 	ld b, a
 	xor a
 	ld [wAudioFadeOutControl], a
-	ld a, $ff
-	ld [wNewSoundID], a
-	call PlaySound
+	call StopAllMusic
 	ld a, [wAudioSavedROMBank]
 	ld [wAudioROMBank], a
 	ld a, b
@@ -2266,6 +2264,16 @@ UpdateGBCPal_OBP1::
 .notGBC
 	pop af
 	ret
+	
+Func_3082:: ;added from pokeyellow - update audio so it doesn't "lag"
+	ld a, [H_LOADEDROMBANK]
+	push af
+	call FadeOutAudio
+	callbs Music_DoLowHealthAlarm
+	callbs Audio1_UpdateMusic
+	pop af
+	call BankswitchCommon
+	ret
 
 ; not zero if an NPC movement script is running, the player character is
 ; automatically stepping down from a door, or joypad states are being simulated
@@ -2733,8 +2741,7 @@ PlayTrainerMusic::
 	ret nz
 	xor a
 	ld [wAudioFadeOutControl], a
-	ld a, $ff
-	call PlaySound
+	call StopAllMusic
 	ld a, BANK(Music_MeetEvilTrainer)
 	ld [wAudioROMBank], a
 	ld [wAudioSavedROMBank], a
@@ -3076,6 +3083,7 @@ BankswitchHome::
 	ld a, [H_LOADEDROMBANK]
 	ld [wBankswitchHomeSavedROMBank], a
 	ld a, [wBankswitchHomeTemp]
+BankswitchCommon::
 	ld [H_LOADEDROMBANK], a
 	ld [MBC1RomBank], a
 	ret

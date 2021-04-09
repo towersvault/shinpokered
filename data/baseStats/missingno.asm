0a2ab79993ebd64fa99621f7b0b2898a4bf91362
jojobear13 <joseph.billo@gmail.com>
2021-04-09 05:26:07 -0500

    make fossil kabutops display again

--- data/baseStats/missingno.asm
+++ data/baseStats/missingno.asm
@@ -1,5 +1,5 @@
 MissingnoBaseStats:
-db FOSSIL_KABUTOPS ; pokedex id
+db MISSINGNO_B5 ; pokedex id
 db 128 ; base hp
 db 128 ; base attack
 db 128 ; base defense
@@ -11,7 +11,7 @@ db 1 ; catch rate
 db 255 ; base exp yield
 INCBIN "pic/other/missingno.pic",0,1 ; $77, sprite dimensions
 dw MissingnoPic
-dw KabutopsPicBack
+dw OldManPic	;use old man for back pic as a placeholder
 ; attacks known at lvl 0
 db WATER_GUN
 db SKY_ATTACK

aforward9600/pokemonpalette

can compile japanese blue

data/wildPokemon/route13.asm
@@ -11,8 +11,18 @@ Route13Mons:
 		db 25,DITTO
 		db 28,GLOOM
 		db 30,GLOOM
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 24, ODDISH
+		db 25, PIDGEY
+		db 27, PIDGEY
+		db 24, VENONAT
+		db 22, ODDISH
+		db 26, VENONAT
+		db 26, ODDISH
+		db 25, DITTO
+		db 28, GLOOM
+		db 30, GLOOM
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db $14
 		db 24,BELLSPROUT
 		db 25,PIDGEY

data/wildPokemon/route24.asm
@@ -11,8 +11,18 @@ Route24Mons:
 		db 13,PIDGEY
 		db 8,ABRA
 		db 12,ABRA
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db  7, CATERPIE
+		db  8, METAPOD
+		db 12, PIDGEY
+		db 12, ODDISH
+		db 13, ODDISH
+		db 10, ABRA
+		db 14, ODDISH
+		db 13,PIDGEY
+		db 8,ABRA
+		db 12,ABRA
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 7,CATERPIE
 		db 8,METAPOD
 		db 12,PIDGEY

data/wildPokemon/route6.asm
@@ -11,8 +11,18 @@ Route6Mons:
 		db 16,PIDGEY
 		db 14,MANKEY
 		db 16,MANKEY
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 13, ODDISH
+		db 13, PIDGEY
+		db 15, PIDGEY
+		db 10, MEOWTH
+		db 12, MEOWTH
+		db 15, ODDISH
+		db 16, ODDISH
+		db 16, PIDGEY
+		db 14, MEOWTH
+		db 16, MEOWTH
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 13,BELLSPROUT
 		db 13,PIDGEY
 		db 15,PIDGEY

data/wildPokemon/route8.asm
@@ -11,8 +11,18 @@ Route8Mons:
 		db 17,GROWLITHE
 		db 15,GROWLITHE
 		db 18,GROWLITHE
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 18,PIDGEY
+		db 18, MEOWTH
+		db 17, SANDSHREW
+		db 16, GROWLITHE
+		db 20, PIDGEY
+		db 20, MEOWTH
+		db 19, SANDSHREW
+		db 17, GROWLITHE
+		db 15, GROWLITHE
+		db 18, GROWLITHE
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 18,PIDGEY
 		db 18,MEOWTH
 		db 17,SANDSHREW

towersvault/shinpokered

data/wildPokemon/route13.asm
@@ -11,8 +11,18 @@ Route13Mons:;joenote - added farfetchd
 		db 28,GLOOM
 		db 30,GLOOM
 		db 27,FARFETCHD
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 24, ODDISH
+		db 25, PIDGEY
+		db 24, VENONAT
+		db 22, ODDISH
+		db 26, VENONAT
+		db 26, ODDISH
+		db 25, DITTO
+		db 28, GLOOM
+		db 30, GLOOM
+		db 27,FARFETCHD
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db $14
 		db 24,BELLSPROUT
 		db 25,PIDGEY
@@ -26,3 +36,4 @@ Route13Mons:;joenote - added farfetchd
 		db 27,FARFETCHD
 	ENDC
 	db $00
+ 

data/wildPokemon/route24.asm
@@ -11,8 +11,18 @@ Route24Mons:;joenote - added oddish/bellsprout
 		db 8,ABRA
 		db 12,BELLSPROUT
 		db 12,ABRA
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db  7, CATERPIE
+		db  8, METAPOD
+		db 12, PIDGEY
+		db 13, ODDISH
+		db 10, ABRA
+		db 14, ODDISH
+		db 13,PIDGEY
+		db 8,ABRA
+		db 12,BELLSPROUT
+		db 12,ABRA
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 7,CATERPIE
 		db 8,METAPOD
 		db 12,PIDGEY

data/wildPokemon/route6.asm
@@ -11,8 +11,18 @@ Route6Mons:;joenote - added squirtle
 		db 14,MANKEY
 		db 10,SQUIRTLE
 		db 16,MANKEY
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 13, ODDISH
+		db 13, PIDGEY
+		db 15, PIDGEY
+		db 12, MEOWTH
+		db 15, ODDISH
+		db 16, ODDISH
+		db 16, PIDGEY
+		db 14, MEOWTH
+		db 10,SQUIRTLE
+		db 16, MEOWTH
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 13,BELLSPROUT
 		db 13,PIDGEY
 		db 15,PIDGEY

data/wildPokemon/route8.asm
@@ -11,8 +11,18 @@ Route8Mons:;joenote - added growlithe/vulpix
 		db 17,GROWLITHE
 		db 15,VULPIX
 		db 18,GROWLITHE
-	ENDC
-	IF (DEF(_BLUE) || DEF(_GREEN))
+	ELIF DEF(_BLUEJP)
+		db 18,PIDGEY
+		db 18, MEOWTH
+		db 17, SANDSHREW
+		db 16, GROWLITHE
+		db 20, PIDGEY
+		db 20, MEOWTH
+		db 19, SANDSHREW
+		db 17, GROWLITHE
+		db 15, VULPIX
+		db 18, GROWLITHE
+	ELIF (DEF(_BLUE) || DEF(_GREEN))
 		db 18,PIDGEY
 		db 18,MEOWTH
 		db 17,SANDSHREW
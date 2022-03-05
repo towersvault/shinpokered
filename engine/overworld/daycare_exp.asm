07a1ba58e7216457bf8645dc5af6690ba8f8f675
jojobear13 <joseph.billo@gmail.com>
2022-03-05 17:59:23 -0600

    make daycare exp more modular in code

--- engine/overworld/daycare_exp.asm
+++ engine/overworld/daycare_exp.asm
@@ -2,22 +2,28 @@ IncrementDayCareMonExp:
 	ld a, [wDayCareInUse]
 	and a
 	ret z
+
+	push bc
+	ld bc, $0001
 	ld hl, wDayCareMonExp + 2
-	inc [hl]
-	ret nz
-	dec hl
-	inc [hl]
-	ret nz
-	dec hl
-;	inc [hl]
-;	ld a, [hl]
-;	cp $50
-;	ret c
-;	ld a, $50
-;	ld [hl], a
+	
+	;add exp 
+	ld a, c	
+	add [hl]
+	ld [hld], a
+	ld a, b
+	adc [hl]
+	ld [hld], a
+	ld a, 0
+	adc [hl]
+	ld [hl], a
+	jr nc, .return
+	
 ;joenote - set the cap at $FFFFFF exp
-	ld a, [hl]
-	cp $FF
-	ret z
-	inc [hl]
+	ld a, $FF
+	ld [hli], a
+	ld [hli], a
+	ld [hl], a
+.return
+	pop bc
 	ret

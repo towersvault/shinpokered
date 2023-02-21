a9c5b70f70ad051d2183bd9ec820dd5a043d9ce2
jojobear13 <jojothebear13@gmail.com>
2023-02-21 10:04:50 -0600

    hotfix for certain house interiors

--- main.asm
+++ main.asm
@@ -410,8 +410,8 @@ LavenderHouse1Blocks:
 LavenderHouse2Blocks:
 CeruleanHouse1Blocks:
 PewterHouse1Blocks:
-PewterHouse2Blocks:
-ViridianHouseBlocks: INCBIN "maps/viridianhouse.blk"
+PewterHouse2Blocks: INCBIN "maps/viridianhouse.blk"
+ViridianHouseBlocks: INCBIN "maps/viridianhouse_new.blk"	;joenote - sectioning this off
 
 CeladonMansion5Blocks:
 SchoolBlocks: INCBIN "maps/school.blk"

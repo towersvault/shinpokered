bb6dbe2744737fd24015d707212c08dcffa67490
jojobear13 <jojothebear13@gmail.com>
2023-09-05 20:45:16 -0500

    trainer ai routine 3 now looks for x-accuracy on ohko moves

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -941,7 +941,7 @@ AIMoveChoiceModification3:
 	jr nz, .skipout3	;skip ahead if not ohko move
 	ld a, [wEnemyBattleStatus2]
 	bit USING_X_ACCURACY, a
-	jr nz, .nextMove	;X-accuracy is being used so ohko move viable
+	jp nz, .nextMove	;X-accuracy is being used so ohko move viable
 	call StrCmpSpeed	;do a speed compare
 	jp c, .nextMove	;ai is fast enough so ohko move viable
 	;else ai is slower so don't bother

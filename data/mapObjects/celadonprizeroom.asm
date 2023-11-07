8129f6b633cac83ddedc4ff7e3ab5f6496fabd11
jojobear13 <jojothebear13@gmail.com>
2023-11-07 00:19:58 -0600

    moved NPC in celadon prize house two spaces to the right

--- data/mapObjects/celadonprizeroom.asm
+++ data/mapObjects/celadonprizeroom.asm
@@ -12,7 +12,7 @@ CeladonPrizeRoomObject:
 
 	db 2 ; objects
 	object SPRITE_BALDING_GUY, 1, 4, STAY, NONE, 1 ; person
-	object SPRITE_GAMBLER, 7, 3, WALK, 2, 2 ; person
+	object SPRITE_GAMBLER, 9, 3, WALK, 2, 2 ; person	;joenote - moved two spaces right to avoid blocking the counter
 
 	; warp-to
 	warp_to 4, 7, CELADON_PRIZE_ROOM_WIDTH
